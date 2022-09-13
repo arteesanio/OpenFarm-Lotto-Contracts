4cece9909dffdfe43cc352b9c18dd4673794e899
Abraham Duno <tresduno@gmail.com>
2022-09-13 10:21:04 -0400

    extra helpers

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -5,6 +5,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20{
     constructor() ERC20("DevToken", "DVT"){
-        _mint(msg.sender,1000*10**18);
+        _mint(msg.sender,10**23);
     }
 }
---
+++ scripts/deploy_-1_vrf.js
@@ -0,0 +1,31 @@
+const { ethers } = require("hardhat");
+require("dotenv").config({ path: ".env" });
+
+async function main() {
+  // URL from where we can extract the metadata
+  // const metadataURL = "ipfs://.../";
+
+  let signers = await ethers.getSigners()
+  let user0 = signers[0].address
+  const provider = waffle.provider;
+  
+  const user0balanceETH = await provider.getBalance(user0);
+  console.log(user0, ethers.utils.formatEther(user0balanceETH))
+
+  // return
+
+  const CONTRACT_object = await ethers.getContractFactory("FakeVRF");
+  const deployedContract = await CONTRACT_object.deploy();
+  await deployedContract.deployed();
+  console.log("Fake Contract: Fake VRF Resolver | Address:", deployedContract.address);
+  // let init_hash = await deployedContract.INIT_CODE_PAIR_HASH()
+  // console.log("init_hash:", init_hash);
+}
+
+// Call the main function and catch if there is any error
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
